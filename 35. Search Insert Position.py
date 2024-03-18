416cd61cf711d6e3e9165db8ac099f0ca4914e6b
shivani-andapally <161233328+shivani-andapally@users.noreply.github.com>
2024-03-18 14:43:10 +0530

    Create 35. Search Insert Position.py

---
+++ 35. Search Insert Position.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        low,high=0,len(nums)-1
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]==target:
+                return mid
+            if nums[mid]<target:
+                low=mid+1
+            else:
+                high=mid-1
+        return low 
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
